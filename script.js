171f6f311125febb42683210972ada70a8375b60
Julian <jjvll90@gmail.com>
2024-08-18 23:55:31 -0500

    funtional buttons

---
+++ script.js
@@ -0,0 +1,26 @@
+// Get the button elements
+const myButton = document.getElementById("myButton");
+const newestButton = document.getElementById("newest");
+const catalogueButton = document.getElementById("catalogue");
+const contactButton = document.getElementById("contact");
+
+// Add event listeners to the buttons
+myButton.addEventListener("click", function() {
+console.log("My button clicked!");
+// Add more code here to handle the button click
+});
+
+newestButton.addEventListener("click", function() {
+console.log("Newest button clicked!");
+// Add more code here to handle the button click
+});
+
+catalogueButton.addEventListener("click", function() {
+console.log("Catalogue button clicked!");
+// Add more code here to handle the button click
+});
+
+contactButton.addEventListener("click", function() {
+console.log("Contact button clicked!");
+// Add more code here to handle the button click
+});
